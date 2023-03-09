9c5caf0c37895138e8dba549b6ce2cf9a8ae655b
Keisuke <m1457846@icloud.com>
2023-03-09 15:43:38 +0900

    a

--- tetris/block.rb
+++ tetris/block.rb
@@ -1,6 +1,6 @@
 require "pp"
 class Block 
-  attr_accessor :pattern_map, :x, :y, :stopped
+  attr_accessor :pattern_map, :x, :y, :stopped 
   def initialize(mx, my)
     self.x = mx
     self.y = my
@@ -9,42 +9,30 @@ class Block
       [0, 1, 1],
       [0, 1, 0]
     ]
+
     @bottom_limit = Map::MAP_HEIGhT - self.pattern_map.length
     @right_limit = Map::MAP_WIDTH - self.pattern_map.length
     @stopped = false
     @movable = true
+   
   end
 
   def stop
     @movable = false
-    @stopped = true
-    self.y -= 1 if @stopped
+    if @stopped == false
+      self.y -= 1
+      @stopped = true
+    end
+    
   end
 
   def update
-=begin
-    if Input.key_push?(K_K) and self.y < (Map::MAP_HEIGhT - self.pattern_map.length)
-      self.pattern_map = self.pattern_map.transpose.map(&:reverse) 
-      @any += 1
-      p @any # 確認用　後で消します
-    end
- 
-    if @any == 4 
-      @any = 0 
-    elsif @any == 3  
-      @d = 2
-      @r = 0
-    elsif @any == 2 
-      @l = -1
-      @r = 1
-    elsif @any == 1
-      @l = -1
-    elsif @any == 0 
-      @l = 0
-    end
-=end
 
-
+    #------空白の削除--------
+    if @bottom_limit
+      pattern_map.reject! {|i| i == [0, 0, 0,] } #破壊的メソッド 
+      p pattern_map
+    end
     #------自由落下----------
 
     self.y += 1 if @movable
@@ -59,16 +47,8 @@ class Block
 
     self.y += 1 if self.y < @bottom_limit if Input.key_down?(K_DOWN)
     
-
-    #self.y += 0.02 if self.y - @d < (Map::MAP_HEIGhT - self.pattern_map.length)
-    
-
-    #self.y += 0.02 if self.y - @d < (Map::MAP_HEIGhT - self.pattern_map.length)
-    #self.y += 0.05 if Input.key_down?(K_DOWN) and self.y - @d < (Map::MAP_HEIGhT - self.pattern_map.length) and self.y < (Map::MAP_HEIGhT - self.pattern_map.length)
-    #self.x += 0.3 if Input.key_down?(K_RIGHT) and self.x - @r < (Map::MAP_WIDTH - self.pattern_map.length) and self.y < (Map::MAP_HEIGhT - self.pattern_map.length)
-    #self.x -= 0.3 if Input.key_down?(K_LEFT) and self.x + @l > 0 and self.y < (Map::MAP_HEIGhT - self.pattern_map.length)
-  end
   
+  end
   #------操作(右回転 配列の内容を変えています 条件:地面についているか否か)-------------
 
   def rotate_right
